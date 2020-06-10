a8d43e0ccf079f5895d1acd1dcb7bd76ab8de157
Henri Lepic <hlepic@@onestock-retail.com>
2020-06-10 13:56:40 +0200

    endpoint: add update videogame

--- endpoints/videogame.go
+++ endpoints/videogame.go
@@ -21,6 +21,7 @@ func initEndpointVideoGame(db db.Store, r *gin.Engine) {
 	r.GET("/videogames", us.GetAll)
 	r.DELETE("/videogames/:uuid", us.Delete)
 	r.POST("/videogames", us.Create)
+	r.PATCH("/videogames/:uuid", us.Update)
 }
 
 func (su *serviceVideoGame) Get(ctx *gin.Context) {
@@ -66,4 +67,23 @@ func (su *serviceVideoGame) Create(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(200, u)
+}
+
+func (su *serviceVideoGame) Update(ctx *gin.Context) {
+
+	update := make(map[string]interface{})
+	if err := ctx.BindJSON(update); err != nil {
+		log.Println(err)
+		ctx.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	v, err := su.db.UpdateVideoGame(ctx.Param("uuid"), update)
+	if err != nil {
+		log.Println(err)
+		ctx.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+
+	ctx.JSON(200, v)
 }
